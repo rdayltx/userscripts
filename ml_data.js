875667a6f3e627af6ecf00877b1628a267777796
Rodrigo <rdayltx@gmail.com>
2024-12-21 11:07:07 -0300

    define intervalo de data como nome da planilha

--- ml_data.js
+++ ml_data.js
@@ -2,7 +2,7 @@
 // @name         Métricas ML para Planilha
 // @namespace    rodrigodev.com.br
 // @author       DayLight
-// @version      1.8
+// @version      1.9
 // @description  Extrai dados da tabela e exporta para um arquivo XLSX
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mercadolivre.com.br
 // @match        https://www.mercadolivre.com.br/afiliados/*
@@ -126,11 +126,23 @@ function main() {
     }
   }
 
+  function getDateRangeFromSpan() {
+    const spanElement = document.querySelector("#\\:Rmlicq\\:-display-values");
+    if (spanElement) {
+      return spanElement.textContent.trim().replace(/\s+/g, "_");
+    }
+    return null;
+  }
+
   function exportToXLSX() {
-    const date = new Date();
-    const day = date.getDate().toString().padStart(2, "0"); // Adiciona zero à esquerda se necessário
-    const month = (date.getMonth() + 1).toString().padStart(2, "0"); // getMonth retorna de 0 a 11
-    const fileName = `Métricas_ML_${day}-${month}.xlsx`;
+    const dateRange = getDateRangeFromSpan();
+    const fileName = dateRange
+      ? `Métricas_ML_${dateRange}.xlsx`
+      : `Métricas_ML_${new Date().getDate().toString().padStart(2, "0")}-${(
+          new Date().getMonth() + 1
+        )
+          .toString()
+          .padStart(2, "0")}.xlsx`;
 
     // Processa os dados para remover "R$" e configurar colunas como números
     const processedData = allData.map((row, index) => {
