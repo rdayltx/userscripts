0ff68ad951aeb56748a288bf65b7a27ee9d94689
DayLight <rdayltx@gmail.com>
2025-01-08 14:48:57 -0300

    bump version

--- ML_Amz_Clear_URL_Parameters.js
+++ ML_Amz_Clear_URL_Parameters.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          ML, Amz. Clear URL Parameters
 // @namespace     Clear URL Parameters
-// @version       1.3
+// @version       1.4
 // @icon          https://raw.githubusercontent.com/rdayltx/userscripts/refs/heads/master/ML_Amz_Clear_URL_Parameters.ico
 // @description   Remove parâmetros desnecessários de URLs de sites especificos
 // @author        DayLight
